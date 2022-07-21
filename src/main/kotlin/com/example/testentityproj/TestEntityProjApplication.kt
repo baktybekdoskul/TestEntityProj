ee30737e11391410e1ece71b79935be2ab599868
baktybekdoskul <baktybek.doskul@gmail.com>
2022-07-21 10:14:18 +0600

    Initial commit

---
+++ src/main/kotlin/com/example/testentityproj/TestEntityProjApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testentityproj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestEntityProjApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestEntityProjApplication>(*args)
+}
